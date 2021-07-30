86aa3ad1402ccbd81714a79f471a2d528f661f71
Joel <joelventura79@gmail.com>
2021-07-30 13:32:36 -0500

    BATTLEGROUNDS animaciones

---
+++ Js/index.js
@@ -0,0 +1,22 @@
+const audio=document.getElementById('audio')
+const section=document.getElementById('section')
+const fragment=document.createDocumentFragment()
+section.addEventListener('click',(e)=>{
+    audio.play()
+    let posicionX=e.offsetX;
+    let posicionY=e.offsetY;
+    CreateImg(posicionX,posicionY)
+})
+
+const CreateImg=(x,y)=>{
+    const img=document.createElement('img')
+    img.setAttribute('src','/Img/img_hoyo.png')
+    img.style.left=`${x}px`
+    img.style.top=`${y}px`
+    fragment.appendChild(img)
+    section.appendChild(fragment)
+
+    setTimeout(() => {
+        img.remove()
+    }, 4000);
+}
